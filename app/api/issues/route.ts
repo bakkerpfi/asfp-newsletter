f8a57fb8b0a472756d4c8973d320b263b1aa2d95
bakkerpfi <ben@bakkerpfi.com>
2026-06-30 09:45:03 +1200

    check2

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -2,24 +2,36 @@ import { NextResponse } from "next/server";
 import db from "@/lib/database";
 
 export async function GET() {
-  const issues = db.prepare(`
-    SELECT *
-    FROM issues
-  `).all();
+const issues = JSON.parse(
+  JSON.stringify(
+    db.prepare(`
+      SELECT *
+      FROM issues
+    `).all()
+  )
+);
 
-  const articles = db.prepare(`
-    SELECT *
-    FROM articles
-  `).all();
+const articles = JSON.parse(
+  JSON.stringify(
+    db.prepare(`
+      SELECT *
+      FROM articles
+    `).all()
+  )
+);
 
-  const polls = db.prepare(`
-    SELECT *
-    FROM polls
-  `).all();
+const polls = JSON.parse(
+  JSON.stringify(
+    db.prepare(`
+      SELECT *
+      FROM polls
+    `).all()
+  )
+);
 
-  return NextResponse.json({
-    issues,
-    articles,
-    polls,
-  });
+return NextResponse.json({
+  issues,
+  articles,
+  polls,
+});
 }
