d29927fbaea686006c0be555e24261120c1fc76b
bakkerpfi <ben@bakkerpfi.com>
2026-06-30 09:37:34 +1200

    Debug database

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,10 +1,25 @@
 import { NextResponse } from "next/server";
+import db from "@/lib/database";
 
 export async function GET() {
-  return NextResponse.json([
-    {
-      id: 3,
-      title: "Test Issue",
-    },
-  ]);
+  try {
+    const issues = db.prepare(`
+      SELECT *
+      FROM issues
+    `).all();
+
+    return NextResponse.json(issues);
+
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json(
+      {
+        error: String(error),
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
