5065e40731b0a0c852680fedf9b4f60b8ebfed64
bakkerpfi <ben@bakkerpfi.com>
2026-06-30 09:29:00 +1200

    Fix database path for Vercel

--- lib/database.ts
+++ lib/database.ts
@@ -1,5 +1,12 @@
 import Database from "better-sqlite3";
+import path from "path";
 
-const db = new Database("database/asfp.db");
+const dbPath = path.join(
+  process.cwd(),
+  "database",
+  "asfp.db"
+);
+
+const db = new Database(dbPath);
 
 export default db;
